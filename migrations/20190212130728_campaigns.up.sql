4de73bb1f260b6a27d54c8d456411696c5e17377
Aristio Rizki <aristio.rizki@pegadaian.co.id>
2019-02-18 11:38:04 +0700

    add index name in table campaigns and add general api response

--- migrations/20190212130728_campaigns.up.sql
+++ migrations/20190212130728_campaigns.up.sql
@@ -1,15 +1,15 @@
--- Table: campaigns
--- for status 0 --> INACTIVE and 1 --> ACTIVE
-CREATE TABLE IF NOT EXISTS campaigns (
-    id SERIAL PRIMARY KEY NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    description TEXT NOT NULL,
-    start_date TIMESTAMP NOT NULL,
-    end_date TIMESTAMP NOT NULL,
-    status SMALLINT DEFAULT 0,
-    validators JSONB NOT NULL, 
-    updated_at TIMESTAMP DEFAULT NULL,
-    created_at TIMESTAMP DEFAULT NULL
-);
+    -- Table: campaigns
+    -- for status 0 --> INACTIVE and 1 --> ACTIVE
+    CREATE TABLE IF NOT EXISTS campaigns (
+        id SERIAL PRIMARY KEY NOT NULL,
+        name VARCHAR(255) NOT NULL,
+        description TEXT NOT NULL,
+        start_date TIMESTAMP NOT NULL,
+        end_date TIMESTAMP NOT NULL,
+        status SMALLINT DEFAULT 0,
+        validators JSONB NOT NULL, 
+        updated_at TIMESTAMP DEFAULT NULL,
+        created_at TIMESTAMP DEFAULT NULL
+    );
 
-CREATE INDEX index_campaigns ON campaigns (id, start_date, end_date, status);
+    CREATE INDEX index_campaigns ON campaigns (id, name, start_date, end_date, status);
--- models/response.go
+++ models/response.go
@@ -3,4 +3,5 @@ package models
 type Response struct {
 	Status  string `json:"status"`
 	Message string `json:"message"`
+	Data    []interface{}
 }

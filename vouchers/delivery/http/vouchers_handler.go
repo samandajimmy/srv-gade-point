ec882f2ec6474732315eb8624fd422a3e67f37a1
jimmy samanda <samandajimmyr@gmail.com>
2019-05-17 23:50:32 +0700

    initial commit for badai emas voucher gift endpoint

--- vouchers/delivery/http/vouchers_handler.go
+++ vouchers/delivery/http/vouchers_handler.go
@@ -24,21 +24,29 @@ func NewVouchersHandler(echoGroup models.EchoGroup, us vouchers.UseCase) {
 		VoucherUseCase: us,
 	}
 
-	//End Point For CMS
+	// End Point For CMS
 	echoGroup.Admin.POST("/vouchers", handler.CreateVoucher)
 	echoGroup.Admin.GET("/vouchers", handler.GetVouchersAdmin)
 	echoGroup.Admin.GET("/vouchers/:id", handler.GetVoucherAdmin)
 	echoGroup.Admin.POST("/vouchers/upload", handler.UploadVoucherImages)
 	echoGroup.Admin.PUT("/vouchers/status/:id", handler.UpdateStatusVoucher)
 
-	//End Point For External
+	// End Point For External
 	echoGroup.API.GET("/vouchers", handler.GetVouchers)
 	echoGroup.API.GET("/vouchers/:id", handler.GetVoucher)
 	echoGroup.API.POST("/vouchers/buy", handler.VoucherBuy)
 	echoGroup.API.POST("/vouchers/redeem", handler.VoucherRedeem)
 	echoGroup.API.GET("/vouchers/user", handler.GetVouchersUser)
 	echoGroup.API.POST("/vouchers/validate", handler.VoucherValidate)
+	echoGroup.API.POST("/vouchers/badai-emas/gift", handler.VoucherValidate)
+}
 
+// BadaiEmasGift function to give client the right badai emas voucher
+func (vchr *VouchersHandler) BadaiEmasGift(c echo.Context) error {
+	var err error
+	response = models.Response{}
+
+	return c.JSON(getStatusCode(err), response)
 }
 
 // CreateVoucher Create new voucher and generate promo code by stock
